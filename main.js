fc80743038b0938ac59a83a7429d3b9ed2eab049
MauriDev <echanizmauricio@gmail.com>
2023-04-26 16:08:46 -0300

    realizado hasta ejercicio 10

---
+++ main.js
@@ -0,0 +1,39 @@
+//Ejercicio N°1
+const miNombre = `Mauricio`
+
+
+//Ejercicio N°2
+const miApellido = `Echaniz`
+
+
+//Ejercicio N°3
+let miEdad = 27
+
+
+//Ejercicio N°4
+const miMascota = `Bodoque`
+
+
+//Ejercicio N°5
+let edadMascota =4
+
+
+//Ejercicio N°6
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota)
+
+
+//Ejercicio N°7
+const nombreCompleto = `${miNombre} ${miApellido}`
+console.log(nombreCompleto)
+
+//Ejercicio N°8
+let textoPresentacion = `Hola, mi nombre es ${nombreCompleto}, tengo ${miEdad} años, mi mascota se llama ${miMascota}, es un perro y tiene ${edadMascota} meses`
+console.log(textoPresentacion)
+
+
+//Ejercicio N°9
+
+let sumaEdades = miEdad + edadMascota;
+let restaEdades = miEdad - edadMascota;
+let productoEdades = miEdad * edadMascota ;
+let divisionEdades = miEdad / edadMascota ;
